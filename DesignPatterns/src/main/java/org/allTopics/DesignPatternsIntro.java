d7828fe63337f137f446b1e839f7d5c89e3234e6
Parth Trivedi <97606376+ParthTrivedi2000@users.noreply.github.com>
2024-09-26 23:17:56 +0530

    Add files via upload

---
+++ DesignPatterns/src/main/java/org/allTopics/DesignPatternsIntro.java
@@ -0,0 +1,34 @@
+package org.allTopics;
+
+/*
+so what are Design Patterns?
+- solution of multiple times occured problem
+
+
+Types of Design Patterns:-
+Note:- Again stating that, Desing Patterns are nothing but idea. Implementation is up to you, I mean
+how to implement that idea is up to the developer. So Design patterns helps to implement it in structured
+manner. but it's not mandate to use design patterns (saying this just to clarify in mind, it's not mandate
+but good if you implement it in your code).
+
+Creational Design Patterns:-
+- from the name itself, it's self understood, these patterns are related to how Objects should be created
+while developing software.
+- example:- Singletone Design Pattern, Factory Design Patter etc etc...
+
+Structural Design Patterns:-
+- Again from the name itself it is understood, like these are the patterns which helps to structurise
+your code as software has hell lot classes and objects. so basically these patterns helps/ideates how
+classes and objects structured to have it minimal as well as provides all the functionalities we want as
+a user.
+- example :-
+
+Behavioural Design Patterns:-
+- These patterns are providing insights, how Objects should communicate/interact with each other, how services
+should communicate each other.
+- example:-
+
+ */
+
+public class DesignPatternsIntro {
+}
