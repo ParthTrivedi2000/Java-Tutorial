ef00016275a3628b748a85ae63fa56bb061aa72f
Parth Trivedi <97606376+ParthTrivedi2000@users.noreply.github.com>
2026-07-17 10:50:44 +0530

    Add example of map operation on integer list in the form of Question
    Added example to demonstrate the use of map with a stream.

--- JavaBasics/OtherPracticeRepos/StreamQuestions/MapvsFlatmap.java
+++ JavaBasics/OtherPracticeRepos/StreamQuestions/MapvsFlatmap.java
@@ -5,4 +5,13 @@ public class MapvsFlatmap {
     //Q) List<Customer> to list of emails
     // Q) get all the phone numbers in a flattened way i.e. in a single list.
     // testing
+
+    // Q - 1)
+    // I/P :- [1,2,3]
+    // O/P :- ["value is 1","value is 2", "value is 3"]
+    List<Integer> integerList = List.of(1,2,3,4,5);
+    List<String> outputList = integerList.stream()
+                                .map(elem -> "value is" + elem)
+                                .collect(Collectors.toList());
+    system.out.println(outputList);
 }
